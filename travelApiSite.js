9dc54c0974f9cdc40487c7777eb28d187c098fab
Celeste Tan <houseoftan4@gmail.com>
2017-04-24 10:36:01 -0700

    added 5 tourist places to each city

--- travelApiSite.js
+++ travelApiSite.js
@@ -11,43 +11,43 @@ var touristplace
  
     if(currentCity == "new york city"){
     var imageString = "<img src='images/nyc.jpg'>"
-    touristplace = "5"
+    touristplace = "union square, statue of liberty, empire state building, central park, rockefeller center"
 }
    else if(currentCity == "san francisco "){
      imageString = "<img src='images/sf.jpg'>"
-     touristplace = ""
+     touristplace = "golden gate bridge, fishermans wharf, alcatraz, lombard street, cable car system"
   }
    else if(currentCity == "seattle"){
       imageString = "<img src='images/seattle.jpg'>"
-      touristplace = ""
+      touristplace = "space needle, pike place market, chihuly garden and glass, experience music project muesuem, seattle great wheel"
   }
      else if(currentCity == "boston"){
      imageString = "<img src='images/boston.jpg'>"
-     touristplace = ""
+     touristplace = "fanueil hall, fenway park, paul revere house, old north church, new england aquarium"
   }
    else if(currentCity == "los angeles"){
      imageString = "<img src='images/la.jpg'>"
-     touristplace = ""
+     touristplace = "universal studios and citywalk, disneyland, hollywood(sign, walk of fame, wax muesuem, manschinese theater), santa monica(3rd street promenade and pier), LA Live(staples center, grammy mueseum)"
   }
    else if(currentCity == "washington dc"){
       imageString = "<img src='images/washingtondc.jpg'>"
-      touristplace = ""
+      touristplace = "lincoln memorial, national mall, white house, national air and space muesuem, US capitol"
   }
      else if(currentCity == "chicago"){
      imageString = "<img src='images/chicago.jpg'>"
-     touristplace = ""
+     touristplace = "cloud gate, navy pier, willis tower, magnificent mile, millenium park "
   }
  else if(currentCity == "houston"){
       imageString = "<img src='images/houston-tx.jpg'>"
-      touristplace = ""
+      touristplace = "muesuem of fine arts, space center houston, houston muesuem of national sciences, menil collection, downtown aquarium"
   }
      else if(currentCity == "austin"){
      imageString = "<img src='images/austin-tx.jpg'>"
-     touristplace = ""
+     touristplace = "texas state capitol, lady bird lake, barton springs pool, zilker park,lyndon baines johnson library"
   }
      else if(currentCity == "san diego"){
      imageString = "<img src='images/sandiego-midway.jpg'>"
-     touristplace = ""
+     touristplace = "uss midway, seaworld, old town san diego, la jolla cove, balboa park"
   }
   document.getElementById("travelinfocontainer").innerHTML = "Current Time: " + currentCity + " Weather: " + cityWeather + " " + cityTemp + "°F Most Popular Tourist Attractions:" + touristplace
     document.getElementById("imagecontainer").innerHTML = imageString
