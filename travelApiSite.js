eaefe727c6c604aa4c025c4bc6639ad8149bce38
Celeste Tan <houseoftan4@gmail.com>
2017-04-26 08:52:24 -0700

    made image smaller

--- travelApiSite.js
+++ travelApiSite.js
@@ -11,45 +11,45 @@ var touristplace
  
     if(document.getElementById('city').value == "new york city"){
     var imageString = "<img src='images/nyc.jpg'>"
-    touristplace = "times square, statue of liberty, empire state building, central park, rockefeller center"
+    touristplace = "1. times square <br> 2. statue of liberty <br> 3. empire state building <br> 4. central park <br> 5. rockefeller center"
 }
    else if(document.getElementById('city').value == "san francisco"){
      imageString = "<img src='images/sf.jpg'>"
-     touristplace = "golden gate bridge, fishermans wharf, alcatraz, lombard street, cable car system"
+     touristplace = "1. golden gate bridge <br> 2. fishermans wharf <br> 3. alcatraz <br> 4. lombard street <br> 5. cable car system"
   }
   else if(document.getElementById('city').value == "seattle"){
       imageString = "<img src='images/seattle.jpg'>"
-      touristplace = "space needle, pike place market, chihuly garden and glass, experience music project muesuem, seattle great wheel"
+      touristplace = "1. space needle <br> 2. pike place market <br> 3. chihuly garden and glass <br> 4. experience music project mueseum <br> 5. seattle great wheel"
   }
      else if(document.getElementById('city').value == "boston"){
      imageString = "<img src='images/boston.jpg'>"
-     touristplace = "fanueil hall, fenway park, paul revere house, old north church, new england aquarium"
+     touristplace = "1. fanueil hall <br>  2. fenway park <br> 3. paul revere house <br> 4. old north church <br> 5. new england aquarium"
   }
    else if(document.getElementById('city').value == "los angeles"){
      imageString = "<img src='images/la.jpg'>"
-     touristplace = "universal studios and citywalk, disneyland, hollywood(sign, walk of fame, wax muesuem, manschinese theater), santa monica(3rd street promenade and pier), LA Live(staples center, grammy mueseum)"
+     touristplace = "1. universal studios and citywalk <br> 2. disneyland <br> 3. hollywood(sign, walk of fame, wax mueseum, manschinese theater) <br> 4. santa monica(3rd street promenade and pier) <br> 5. LA Live(staples center, grammy mueseum)"
   }
    else if(document.getElementById('city').value == "washington dc"){
       imageString = "<img src='images/washingtondc.jpg'>"
-      touristplace = "lincoln memorial, national mall, white house, national air and space muesuem, US capitol"
+      touristplace = "1. lincoln memorial <br> 2. national mall <br> 3. white house <br> 4. national air and space mueseum <br> 5. US capitol"
   }
      else if(document.getElementById('city').value == "chicago"){
      imageString = "<img src='images/chicago.jpg'>"
-     touristplace = "cloud gate, navy pier, willis tower, magnificent mile, millenium park "
+     touristplace = "1. cloud gate <br> 2. navy pier <br> 3. willis tower <br> 4. magnificent mile <br> 5.  millenium park "
   }
  else if(document.getElementById('city').value == "houston"){
       imageString = "<img src='images/houston-tx.jpg'>"
-      touristplace = "muesuem of fine arts, space center houston, houston muesuem of national sciences, menil collection, downtown aquarium"
+      touristplace = "1. muesuem of fine arts <br> 2. space center houston <br> 3. houston mueseum of national sciences <br> 4. menil collection <br> 5. downtown aquarium"
   }
      else if(document.getElementById('city').value == "austin"){
      imageString = "<img src='images/austin-tx.jpg'>"
-     touristplace = "texas state capitol, lady bird lake, barton springs pool, zilker park,lyndon baines johnson library"
+     touristplace = "1. texas state capitol <br> 2. lady bird lake <br> 3. barton springs pool <br> 4. zilker park <br> 5. lyndon baines johnson library"
   }
      else if(document.getElementById('city').value == "san diego"){
      imageString = "<img src='images/sandiego-midway.jpg'>"
-     touristplace = "uss midway, seaworld, old town san diego, la jolla cove, balboa park"
+     touristplace = "1. uss midway <br> 2. seaworld <br> 3. old town san diego <br> 4. la jolla cove <br> 5. balboa park"
   }
-  document.getElementById("travelinfocontainer").innerHTML = "Current Time: " + currentCity + " Weather: " + cityWeather + " " + cityTemp + "°F Most Popular Tourist Attractions:" + touristplace
+  document.getElementById("travelinfocontainer").innerHTML = "Current Time: " + currentCity + "<br> Weather: " + cityWeather + " " + cityTemp + "°F <br> Most Popular Tourist Attractions:<br>" + touristplace + ""
     document.getElementById("imagecontainer").innerHTML = imageString
  
 } 
